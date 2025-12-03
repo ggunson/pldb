ee2a50df906736ce7dea3c6a24ac4156c13d1f1b
Gillian Gunson <ggunson@users.noreply.github.com>
2025-12-03 12:02:14 -0800

    Parser in PHP (#11)

---
+++ scripts/pldb_mysql.php
@@ -0,0 +1,116 @@
+ <?php
+
+// returns an array of show id numbers -- we're assuming this is always filled out
+function get_all_show_ids($conn){
+  $qry = "SELECT id from shows ORDER BY id DESC";
+  // mysqli_query(connection, query, resultmode)
+  $result = mysqli_query($conn, $qry);
+  // echo "Returned rows are: " . mysqli_num_rows($result);
+  $rows = mysqli_fetch_all($result, MYSQLI_NUM);
+  // Free result set
+  mysqli_free_result($result);
+  return $rows;
+}
+
+function get_show_id($conn, $theme){
+  /* create a prepared statement */
+  $stmt = mysqli_prepare($conn, "SELECT id FROM shows WHERE theme = ?");
+  /* bind parameters for markers */
+  mysqli_stmt_bind_param($stmt, "s", $theme);
+  /* execute query */
+  mysqli_stmt_execute($stmt);
+  /* bind result variables */
+  mysqli_stmt_bind_result($stmt, $id);
+  /* fetch value and close */
+  mysqli_stmt_fetch($stmt);
+  mysqli_stmt_close($stmt);
+  
+  return $id;
+}
+
+// returns affected_rows
+function insert_new_show($conn, $id, $theme, $airdate, $archive_link){
+  $stmt = mysqli_prepare($conn, "INSERT INTO shows (id, theme, airdate, archivelink) VALUES (?, ?, ?, ?)");
+  mysqli_stmt_bind_param($stmt, "isss", $id, $theme, $airdate, $archive_link);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
+  return mysqli_affected_rows($conn);
+}
+
+// returns artist_id
+function get_artist_id($conn, $name){
+  $query = "SELECT id FROM artists WHERE name = ?";
+  $stmt = mysqli_prepare($conn, $query);
+  mysqli_stmt_bind_param($stmt, "s", $name);
+  mysqli_stmt_execute($stmt);
+
+  /* bind result variables */
+  mysqli_stmt_bind_result($stmt, $id);
+
+  mysqli_stmt_fetch($stmt);
+  mysqli_stmt_close($stmt);
+  return $id;
+}
+
+// returns artist_id
+function insert_new_artist($conn, $name){
+  $query = "INSERT INTO artists (name) VALUES (?)";
+  $stmt = mysqli_prepare($conn, $query);
+  mysqli_stmt_bind_param($stmt, "s", $name);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
+  return mysqli_insert_id($conn);
+}
+
+// returns track id
+function get_track_id($conn, $artist_id, $title){
+  $query = "SELECT id FROM tracks WHERE artist_id = ? AND title = ?";
+  $stmt = null;
+  $stmt = mysqli_prepare($conn, $query);
+  mysqli_stmt_bind_param($stmt, "is", $artist_id, $title);
+  
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_bind_result($stmt, $id);
+
+  mysqli_stmt_fetch($stmt);
+  mysqli_stmt_close($stmt);
+  return $id;
+}
+
+// returns track_id
+function insert_new_track($conn, $artist_id, $title, $year){
+  $query = "INSERT INTO tracks (artist_id, title, year) VALUES (?, ?, ?)";
+  $stmt = mysqli_prepare($conn, $query);
+  mysqli_stmt_bind_param($stmt, "isi", $artist_id, $title, $year);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
+  return mysqli_insert_id($conn);
+}
+
+// returns play id
+function get_play_id($conn, $show_id, $track_id){
+  $query = "SELECT id FROM plays WHERE show_id = ? AND track_id = ?";
+  $stmt = mysqli_prepare($conn, $query);
+  mysqli_stmt_bind_param($stmt, "ii", $show_id, $track_id);
+  mysqli_stmt_execute($stmt);
+
+  /* bind result variables */
+  mysqli_stmt_bind_result($stmt, $id);
+
+  mysqli_stmt_fetch($stmt);
+  mysqli_stmt_close($stmt);
+  return $id;
+}
+
+// returns track_id
+function insert_new_play($conn, $show_id, $track_id, $suggesters, $comment){
+  $query = "INSERT INTO plays (show_id, track_id, suggesters, comment) VALUES (?, ?, ?, ?)";
+  $stmt = mysqli_prepare($conn, $query);
+  mysqli_stmt_bind_param($stmt, "iiss", $show_id, $track_id, $suggesters, $comment);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
+  return mysqli_insert_id($conn);
+}
+
+
+?>
